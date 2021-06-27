92125adfc0c2e2a481b46c753eb0c5128bd64d86
DarjanCrncic <darjancr@gmail.com>
2021-06-27 21:58:49 +0200

    Added zipkin and rabbitmq. Finished endpoint, created the docker-compose file.

--- currency-exchange-service/src/main/java/com/darjan/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/darjan/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.darjan.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 	
 	private Environment environment;
 	private CurrencyExchangeRepository currencyExchangeRepository;
@@ -19,6 +23,9 @@ public class CurrencyExchangeController {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
+		
 		CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
 		
 		if(currencyExchange == null) {
